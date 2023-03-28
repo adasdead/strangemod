53503a104d27a727032a222d43b19b266b291f48
adasdead <adasdead.dev@gmail.com>
2023-03-28 20:12:10 +0600

    fix

--- src/main/java/com/beetw/strangemod/block/MaxwellBlock.java
+++ src/main/java/com/beetw/strangemod/block/MaxwellBlock.java
@@ -8,15 +8,12 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
-import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.stream.Stream;
-
+@SuppressWarnings("deprecation")
 @RegisterBlockItem
 public class MaxwellBlock extends Block {
     private static final Properties PROPERTIES = AbstractBlock.Properties
@@ -34,14 +31,6 @@ public class MaxwellBlock extends Block {
                                         @NotNull IBlockReader blockReader,
                                         @NotNull BlockPos pos,
                                         @NotNull ISelectionContext context) {
-        return Stream.of(
-                Block.box(4.5, 4.5, 4.5, 11.5, 11.5, 11.5),
-                Block.box(12, 4, 0, 16, 12, 4),
-                Block.box(0, 4, 12, 4, 12, 16),
-                Block.box(12, 4, 12, 16, 12, 16),
-                Block.box(0, 4, 0, 4, 12, 4),
-                Block.box(0, 12, 0, 16, 16, 16),
-                Block.box(0, 0, 0, 16, 4, 16)
-        ).reduce((v1, v2) -> VoxelShapes.join(v1, v2, IBooleanFunction.OR)).get();
+        return Block.box(3, 2, 3, 14, 9, 13);
     }
 }
