d9ed90440a432669305e42eccf07e7d767ff9ffc
adasdead <adasdead.dev@gmail.com>
2023-08-26 02:19:03 +0600

    Ultra small fix

--- src/main/java/com/strangedevs/strangemod/client/screens/ChipsStandScreen.java
+++ src/main/java/com/strangedevs/strangemod/client/screens/ChipsStandScreen.java
@@ -63,8 +63,8 @@ public class ChipsStandScreen extends ContainerScreen<ChipsStandScreen.Menu> {
         private static final int PLAYER_INVENTORY_COLUMN_COUNT = 9;
         private static final int PLAYER_INVENTORY_SLOT_COUNT = PLAYER_INVENTORY_COLUMN_COUNT * PLAYER_INVENTORY_ROW_COUNT;
         private static final int VANILLA_SLOT_COUNT = HOTBAR_SLOT_COUNT + PLAYER_INVENTORY_SLOT_COUNT;
-        private static final int TE_INVENTORY_FIRST_SLOT_INDEX = VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT;
         private static final int VANILLA_FIRST_SLOT_INDEX = 0;
+        private static final int TE_INVENTORY_FIRST_SLOT_INDEX = VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT;
         // THIS YOU HAVE TO DEFINE!
         private static final int TE_INVENTORY_SLOT_COUNT = 4;  // must be the number of slots you have!
         private final ChipsStandBlockEntity blockEntity;
