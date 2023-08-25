dd69c69a4f162c96e8e5bb8e064afc84a6874dd5
adasdead <adasdead.dev@gmail.com>
2023-08-26 02:51:26 +0600

    Fixed ItzKyKySHkaStatueBlock display

--- src/main/java/com/strangedevs/strangemod/event/ClientSetupEvent.java
+++ src/main/java/com/strangedevs/strangemod/event/ClientSetupEvent.java
@@ -5,11 +5,10 @@ import com.strangedevs.strangemod.client.renderer.block.ChipsStandRenderer;
 import com.strangedevs.strangemod.client.renderer.entity.NokiaBoxRenderer;
 import com.strangedevs.strangemod.client.screens.ChipsStandScreen;
 import com.strangedevs.strangemod.item.ChipsItem;
-import com.strangedevs.strangemod.registry.ModBlockEntities;
-import com.strangedevs.strangemod.registry.ModEntityTypes;
-import com.strangedevs.strangemod.registry.ModItems;
-import com.strangedevs.strangemod.registry.ModMenuTypes;
+import com.strangedevs.strangemod.registry.*;
 import net.minecraft.client.gui.ScreenManager;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.item.ItemModelsProperties;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
@@ -26,5 +25,6 @@ public class ClientSetupEvent {
         RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.NOKIA_BOX.get(), NokiaBoxRenderer::new);
         ScreenManager.register(ModMenuTypes.CHIPS_STAND_MENU.get(), ChipsStandScreen::new);
         ClientRegistry.bindTileEntityRenderer(ModBlockEntities.CHIPS_BLOCK_ENTITY.get(), ChipsStandRenderer::new);
+        RenderTypeLookup.setRenderLayer(ModBlocks.ITZKYKYSHKA_STATUE.get(), RenderType.cutout());
     }
 }
